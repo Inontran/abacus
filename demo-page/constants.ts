03586556e49cc098d761b5046e3d6e316bece47e
Inontran <pakim.wesi@gmail.com>
2021-10-10 18:24:22 +0900

    #5 Добавлен файл 'constants.ts'.

---
+++ demo-page/constants.ts
@@ -0,0 +1,26 @@
+export const SLIDER_CONFIGS: Array<AbacusOptions> = [
+  {
+    min: -10,
+    max: 9,
+    step: 2,
+    values: [-4, 6],
+    range: true,
+    scale: true,
+    tooltip: true,
+  },
+  {
+    animate: 'fast',
+    max: 1000,
+    step: 0.5,
+    value: 500,
+    range: 'max',
+  },
+  {
+    min: 10,
+    max: 100,
+    step: 1,
+    range: 'min',
+    orientation: 'vertical',
+    scale: true,
+  },
+];
--- demo-page/demo-page.ts
+++ demo-page/demo-page.ts
@@ -1,33 +1,8 @@
 import AbacusDemo from './AbacusDemo';
+import { SLIDER_CONFIGS } from './constants';
 
 $(() => {
   const $sliders = $('.js-sliders');
-  const SLIDER_CONFIGS: Array<AbacusOptions> = [
-    {
-      min: -10,
-      max: 9,
-      step: 2,
-      values: [-4, 6],
-      range: true,
-      scale: true,
-      tooltip: true,
-    },
-    {
-      animate: 'fast',
-      max: 1000,
-      step: 0.5,
-      value: 500,
-      range: 'max',
-    },
-    {
-      min: 10,
-      max: 100,
-      step: 1,
-      range: 'min',
-      orientation: 'vertical',
-      scale: true,
-    },
-  ];
 
   for (let i = 2; i <= SLIDER_CONFIGS.length; i += 1) {
     const $newSliderCard = $('.js-sliders__item:first', $sliders).clone();
